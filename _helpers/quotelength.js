b1bdcb8864203917e02342dabc506e17a3f0fec7
Kenneth Ormandy <kenneth@chloi.io>
2015-05-07 23:30:05 -0700

    Improves blockquote length helper

--- _helpers/quotelength.js
+++ _helpers/quotelength.js
@@ -5,7 +5,14 @@ module.exports = (function(content) {
   $('blockquote').map(function(i, el) {
     // this === el
     var $self = $(this);
-    $self.attr('data-length', $self.text().length);
+    var textLength = $self.text().length;
+    if(textLength > 600) {
+      $self.attr('data-length', textLength).addClass('blockquote blockquote--long');
+    } else if(textLength > 300) {
+      $self.attr('data-length', textLength).addClass('blockquote blockquote--medium');
+    } else {
+      $self.attr('data-length', textLength).addClass('blockquote blockquote--short');
+    }
   });
   return $.html();
 });
