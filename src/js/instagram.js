45a844640fa4b55d1b73724aaff17b59ed94d425
Kenneth Ormandy <kenneth@chloi.io>
2015-08-02 14:13:38 -0700

    Adds basic Instagram feed

---
+++ src/js/instagram.js
@@ -0,0 +1,25 @@
+dug({
+  target: 'js-instagram',
+  cacheExpire: '3600000',
+  endpoint: 'https://api.instagram.com/v1/tags/fieldtripam/media/recent?client_id=fad5f6d6043a4c9b9e456c7b0e0b359e',
+  beforeRender: function (feed) {
+    console.log(feed.data)
+    // feed.data.forEach(function (gram, i) {
+    //   if(gram.user.username = 'rileycran' || (gram.location && gram.location.name === 'The Main') || (typeof gram.location === 'undefined' && gram.tags.length <= 3)) {
+    //   } else {
+    //     feed.data.splice(i, 1)
+    //   }
+    // })
+    // return feed
+  },
+  template: '\
+    {{#data}}\
+      <li>\
+        <a href="{{link}}" class="photo" data-content="{{caption.text}}">\
+          <img src="{{images.low_resolution.url}}" alt="">\
+          <!-- <p><strong>{{user.username}}</strong> {{caption.text}}</p> -->\
+        </a>\
+      </li>\
+    {{/data}}'
+    // TODO Could put “more…” link here
+  })
