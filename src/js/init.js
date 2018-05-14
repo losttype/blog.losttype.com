e1743f9ffc6320d25bc8862813022d67b3869609
kennethormandy <hello@kennethormandy.com>
2018-05-14 13:05:07 -0700

    Adds new place to load Prospectus from, updates CSS accordingly

--- src/js/init.js
+++ src/js/init.js
@@ -2,30 +2,30 @@
 if ('querySelector' in window.document) {
   var mustard = true
 
-  if (typeof sessionStorage.loadedMoriston === 'undefined' || sessionStorage.loadedMoriston !== true) {
-    loadCSS('http://fonts.kennethormandy.com/css/moriston.css')
-
-    var observeMoriston = new FontFaceObserver('Moriston')
-      .check()
-      .then(function() {
-        setFontsLoaded('Moriston')
-      })
-  }
-
-  // if (typeof sessionStorage.loadedProspectus === 'undefined' || sessionStorage.loadedProspectus !== true) {
-  //   loadCSS('../css/fonts.css')
+  // if (typeof sessionStorage.loadedMoriston === 'undefined' || sessionStorage.loadedMoriston !== true) {
+  //   loadCSS('http://fonts.kennethormandy.com/css/moriston.css')
   //
-  //   var observeProspectus = new FontFaceObserver('Prospectus')
+  //   var observeMoriston = new FontFaceObserver('Moriston')
   //     .check()
   //     .then(function() {
-  //       setFontsLoaded('Prospectus')
+  //       setFontsLoaded('Moriston')
   //     })
   // }
-
-  var setFontsLoaded = function (name) {
-    var html = document.documentElement
-    html.classList.add('wf-' + name.toLowerCase())
-    sessionStorage['loaded' + name] = true
-    // console.log('Loaded ' + name)
-  }
+  //
+  // // if (typeof sessionStorage.loadedProspectus === 'undefined' || sessionStorage.loadedProspectus !== true) {
+  // //   loadCSS('../css/fonts.css')
+  // //
+  // //   var observeProspectus = new FontFaceObserver('Prospectus')
+  // //     .check()
+  // //     .then(function() {
+  // //       setFontsLoaded('Prospectus')
+  // //     })
+  // // }
+  //
+  // var setFontsLoaded = function (name) {
+  //   var html = document.documentElement
+  //   html.classList.add('wf-' + name.toLowerCase())
+  //   sessionStorage['loaded' + name] = true
+  //   // console.log('Loaded ' + name)
+  // }
 }
